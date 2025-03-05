12a1d1e36b38c28451f6834112dd58f8a02c3f97
AdhiKrisna <krisnahmbtn@gmail.com>
2025-03-05 18:41:03 +0700

    update: favorite button

--- lib/pages/cat_list_page.dart
+++ lib/pages/cat_list_page.dart
@@ -3,8 +3,15 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:tugas2/constants/constant_colors.dart';
 import 'package:tugas2/data/cats.dart';
 
-class CatListPage extends StatelessWidget {
+class CatListPage extends StatefulWidget {
   const CatListPage({super.key});
+
+  @override
+  State<CatListPage> createState() => _CatListPageState();
+}
+
+class _CatListPageState extends State<CatListPage> {
+  List<bool> isFavorite = List.generate(catList.length, (index) => false);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,67 +47,90 @@ class CatListPage extends StatelessWidget {
                     arguments: catList[index],
                   );
                 },
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                    color: ConstantColors.blueCardHeader,
-                    borderRadius: BorderRadius.circular(30),
-                    border: Border.all(
-                      color: ConstantColors.goldFont,
-                      width: 2,
-                    ),
-                  ),
-                  child: Column(
-                    children: [
-                      const SizedBox(height: 10),
-                      Text(
-                        catList[index].name,
-                        style: GoogleFonts.poppins(
-                          color: ConstantColors.whiteFont,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w700,
+                child: Stack(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        color: ConstantColors.blueCardHeader,
+                        borderRadius: BorderRadius.circular(30),
+                        border: Border.all(
+                          color: ConstantColors.goldFont,
+                          width: 2,
                         ),
                       ),
-                      Text(
-                        catList[index].breed,
-                        style: GoogleFonts.poppins(
-                          color: ConstantColors.whiteFont,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      Expanded(
-                        child: ClipOval(
-                          child: Image.network(
-                            catList[index].pictureUrl,
-                            width: MediaQuery.of(context).size.width * 0.75,
-                            height: MediaQuery.of(context).size.height * 0.3,
-                            fit: BoxFit.cover,
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 10),
+                          Text(
+                            catList[index].name,
+                            style: GoogleFonts.poppins(
+                              color: ConstantColors.whiteFont,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w700,
+                            ),
                           ),
-                        ),
+                          Text(
+                            catList[index].breed,
+                            style: GoogleFonts.poppins(
+                              color: ConstantColors.whiteFont,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Expanded(
+                            child: ClipOval(
+                              child: Image.network(
+                                catList[index].pictureUrl,
+                                width: MediaQuery.of(context).size.width * 0.75,
+                                height:
+                                    MediaQuery.of(context).size.height * 0.3,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            'Age: ${catList[index].age}',
+                            style: GoogleFonts.poppins(
+                              color: ConstantColors.whiteFont,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            'Gender: ${catList[index].sex}',
+                            style: GoogleFonts.poppins(
+                              color: ConstantColors.whiteFont,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                        ],
                       ),
-                      const SizedBox(height: 10),
-                      Text(
-                        'Age: ${catList[index].age}',
-                        style: GoogleFonts.poppins(
-                          color: ConstantColors.whiteFont,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500,
+                    ),
+                    Positioned(
+                      right: 10,
+                      bottom: 10,
+                      child: IconButton(
+                        icon: Icon(
+                          size: 40,
+                          isFavorite[index]
+                              ? Icons.favorite
+                              : Icons.favorite_border,
+                          color: ConstantColors.goldFont,
                         ),
+                        onPressed: () {
+                          setState(() {
+                            isFavorite[index] = !isFavorite[index];
+                          });
+                        },
                       ),
-                      const SizedBox(height: 10),
-                      Text(
-                        'Gender: ${catList[index].sex}',
-                        style: GoogleFonts.poppins(
-                          color: ConstantColors.whiteFont,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               );
             },
